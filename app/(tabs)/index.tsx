19a59c4d71c3783621c33ac23e2022bc12f6ef43
AgamSingh4756 <agamveer4756.be22@chitkara.edu.in>
2025-09-05 21:07:19 +1000

    home page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,16 +1,41 @@
-// app/(tabs)/index.tsx
-import React from 'react';
-import { Image, SafeAreaView, StatusBar, StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { 
+  Image, 
+  SafeAreaView, 
+  StatusBar, 
+  StyleSheet, 
+  Text, 
+  View, 
+  Pressable 
+} from 'react-native';
+import { useRouter } from 'expo-router';
 
 export default function HomeScreen() {
+  const [showButton, setShowButton] = useState(false);
+  const router = useRouter();
+  
   return (
     <SafeAreaView style={styles.safe}>
       <StatusBar barStyle="light-content" backgroundColor="#000" />
-      <View style={styles.container}>
-        <Image source={require('../../assets/images/company_logo.png')} style={styles.logo} />
-        <Text style={styles.title}>WalkBuddy</Text>
-        <Text style={styles.subtitle}>InnovAIte</Text>
-      </View>
+      <Pressable style={{ flex: 1 }} onPress={() => setShowButton(true)}>
+        <View style={styles.container}>
+          <Image 
+            source={require('../../assets/images/company_logo.png')} 
+            style={styles.logo} 
+          />
+          <Text style={styles.title}>WalkBuddy</Text>
+          <Text style={styles.subtitle}>InnovAIte</Text>
+
+          {showButton && (
+            <Pressable 
+              style={styles.customButton} 
+              onPress={() => router.push('/home')}
+            >
+              <Text style={styles.customButtonText}>Start Walking</Text>
+            </Pressable>
+          )}
+        </View>
+      </Pressable>
     </SafeAreaView>
   );
 }
@@ -31,5 +56,18 @@ const styles = StyleSheet.create({
     fontSize: 22,
     fontWeight: '800',
     color: '#FFA500',
+  },
+  customButton: {
+    position: 'absolute',
+    bottom: 100,
+    backgroundColor: '#4b4444ff', 
+    paddingVertical: 12,
+    paddingHorizontal: 28,
+    borderRadius: 8,
+  },
+  customButtonText: {
+    color: '#FFA500',
+    fontSize: 18,
+    fontWeight: '700',
   },
 });
